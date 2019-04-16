b25fb58ec3fa1d5fcf69180bcb28bc3f36227ce8
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-04-16 12:50:48 +0300

    Methods documented

--- src/TronLink.js
+++ src/TronLink.js
@@ -1,25 +1,54 @@
+/**
+ * TronLink extension interaction functionality
+ */
 export default class TronLink {
 
+  /**
+   * Initiates TronLink support object.
+   * 
+   * @param {Object} tronWeb tronWeb entity object
+   *           (details: https://github.com/tronprotocol/tron-web)
+   */
   constructor(tronWeb) {
     this.tronWeb = tronWeb
   }
 
+  /**
+   * Checks if TronLink browser extension is installed
+   */
   isInstalled() {
     return !!this.tronWeb
   }
 
+  /**
+   * Checks if user is logged in to the TronLink plugin
+   */
   isLoggedIn() {
     return this.tronWeb && this.tronWeb.ready
   }
 
+  /**
+   * Checks if user is logged in to the TronLink plugin.
+   * Alias for isLoggedIn() method.
+   */
   isUnlocked() {
     return this.isLoggedIn()
   }
 
+  /**
+   * Returns logged in user Tron address
+   */
   getAccountAddress() {
     return this.tronWeb.defaultAddress.base58
   }
 
+  /**
+   * Converts Tron address from one format to another.
+   * 
+   * @param {String, Number} address Address to convert
+   * @param {String} fromFormat From format string
+   * @param {String} toFormat To format string
+   */
   convertAddress(address, fromFormat, toFormat) {
     switch (toFormat) {
       case 'base58':
